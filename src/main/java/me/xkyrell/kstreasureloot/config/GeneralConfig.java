881421197d0e13d68424387e38a3f11572a52155
Xkyrell <xkyrell@gmail.com>
2024-12-07 20:14:55 +0200

    Add GeneralConfig

---
+++ src/main/java/me/xkyrell/kstreasureloot/config/GeneralConfig.java
@@ -0,0 +1,115 @@
+package me.xkyrell.kstreasureloot.config;
+
+import lombok.NonNull;
+import me.xkyrell.kstreasureloot.loot.*;
+import me.xkyrell.kstreasureloot.loot.impl.SimpleLoot;
+import me.xkyrell.kstreasureloot.loot.service.LootService;
+import org.bukkit.World;
+import org.bukkit.block.Biome;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.Plugin;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+public class GeneralConfig extends Config {
+
+    private final Plugin plugin;
+    private final LootService lootService;
+
+    public GeneralConfig(Plugin plugin, LootService lootService) {
+        super(plugin, "config");
+
+        this.plugin = plugin;
+        this.lootService = lootService;
+        updateOrLoad();
+    }
+
+    @Override
+    public void reload() {
+        super.reload();
+        updateOrLoad();
+    }
+
+    private void updateOrLoad() {
+        ConfigurationSection customLootSection = getSource().getConfigurationSection("custom-loot");
+        if (customLootSection == null) {
+            return;
+        }
+
+        lootService.unregisterAll();
+        for (String key : customLootSection.getKeys(false)) {
+            ConfigurationSection lootSection = customLootSection.getConfigurationSection(key);
+            if (lootSection == null) {
+                continue;
+            }
+
+            World world = loadWorld(lootSection.getString("world"));
+            Biome biome = loadBiome(lootSection.getString("biome"));
+
+            List<LootItem> lootItems = loadItems(lootSection.getConfigurationSection("items"));
+
+            lootService.register(new SimpleLoot(key, world, biome, lootItems));
+        }
+    }
+
+    private World loadWorld(@NonNull String worldName) {
+        World world = plugin.getServer().getWorld(worldName);
+        if (world == null) {
+            throw new IllegalArgumentException("World not found: " + worldName);
+        }
+        return world;
+    }
+
+    private Biome loadBiome(@NonNull String biomeName) {
+        try {
+            return Biome.valueOf(biomeName.toUpperCase());
+        }
+        catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException("Invalid biome name: " + biomeName, e);
+        }
+    }
+
+    private List<LootItem> loadItems(ConfigurationSection itemsSection) {
+        if (itemsSection == null) {
+            return Collections.emptyList();
+        }
+
+        return itemsSection.getMapList("").stream()
+                .map(itemMap -> {
+                    ConfigurationSection itemSection = (ConfigurationSection) itemMap.get("item");
+                    if (itemSection == null) {
+                        return null;
+                    }
+                    ItemStack itemStack = ItemStack.deserialize(itemSection.getValues(false));
+                    double probability = (double) itemMap.get("probability");
+
+                    return new LootItem(itemStack, probability);
+                })
+                .filter(Objects::nonNull)
+                .toList();
+    }
+
+    public void saveLoot(Loot loot) {
+        ConfigurationSection customLootSection = getSource().getConfigurationSection("custom-loot");
+        if (customLootSection == null) {
+            customLootSection = getSource().createSection("custom-loot");
+        }
+
+        ConfigurationSection lootSection = customLootSection.createSection(loot.getName());
+        lootSection.set("world", loot.getWorld().getName());
+        lootSection.set("biome", loot.getBiome().name());
+
+        List<Map<String, Object>> serializedItems = loot.getItems().stream()
+                .map(lootItem -> Map.of(
+                        "item", lootItem.itemStack().serialize(),
+                        "probability", lootItem.probability()
+                ))
+                .toList();
+
+        lootSection.set("items", serializedItems);
+        save();
+    }
+}
