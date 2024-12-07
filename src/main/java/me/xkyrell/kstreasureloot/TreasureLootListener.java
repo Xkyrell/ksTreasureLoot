34473755b38b7148af78578eac9da19f74ab57c6
Xkyrell <xkyrell@gmail.com>
2024-12-07 13:54:54 +0200

    Add listener for handling

---
+++ src/main/java/me/xkyrell/kstreasureloot/TreasureLootListener.java
@@ -0,0 +1,44 @@
+package me.xkyrell.kstreasureloot;
+
+import lombok.RequiredArgsConstructor;
+import me.xkyrell.kstreasureloot.loot.Loot;
+import me.xkyrell.kstreasureloot.loot.LootItem;
+import me.xkyrell.kstreasureloot.loot.service.LootResolver;
+import org.bukkit.Location;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.world.LootGenerateEvent;
+import org.bukkit.inventory.ItemStack;
+import java.util.List;
+import java.util.Random;
+
+@RequiredArgsConstructor
+public class TreasureLootListener implements Listener {
+
+    private final LootResolver resolver;
+    private final Random random = new Random();
+
+    @EventHandler(priority = EventPriority.LOW)
+    public void onGenerate(LootGenerateEvent event) {
+        for (Loot loot : resolver.getLoot()) {
+            Location location = event.getLootContext().getLocation();
+            if (!location.getWorld().equals(loot.getWorld())) {
+                continue;
+            }
+
+            if (!location.getBlock().getBiome().equals(loot.getBiome())) {
+                continue;
+            }
+
+            event.getLoot().addAll(generateCustomLoot(loot));
+        }
+    }
+
+    private List<ItemStack> generateCustomLoot(Loot loot) {
+        return loot.getItems().stream()
+                .filter(lootItem -> random.nextDouble() < lootItem.probability())
+                .map(LootItem::itemStack)
+                .toList();
+    }
+}
